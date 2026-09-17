460a1c63f66639359e8f467c1fbacaa37c14e149
Peter D. Zaffina <pdzaffina@gmail.com>
2026-09-16 20:16:21 -0500

    Add quote by Rebecca Welton about the Eiffel Tower

--- richmond-quotes-data.js
+++ richmond-quotes-data.js
@@ -45,5 +45,6 @@ const quotes = [
   { id: 40, n: 500,  text: "Believe", name: "Diamond Dogs", role: "taped above the door", special: 'blue' },
   { id: 41,  n: 93,  text: "Stop letting yesterday get in the way of today.", name: "Rebecca Welton", role: "Club Owner" },
   { id: 42,  n: 308,  text: "Find out before you flip out.", name: "Leslie Higgins", role: "Chief of Football Ops" },
+  { id: 43,  n: 308,  text: "The Eiffel Tower is just a lamp post with a publicist.", name: "Rebecca Welton", role: "Club Owner" },
   
 ];
