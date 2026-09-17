94530ab2f81110c0085fce06ec6bc22ff85faea3
Peter D. Zaffina <pdzaffina@gmail.com>
2026-09-16 20:08:16 -0500

    Add quote from Leslie Higgins

--- richmond-quotes-data.js
+++ richmond-quotes-data.js
@@ -44,5 +44,6 @@ const quotes = [
   { id: 39,  n: 80,  text: "F***!", name: "Roy Kent", role: "Captain/Assistant Coach" },
   { id: 40, n: 500,  text: "Believe", name: "Diamond Dogs", role: "taped above the door", special: 'blue' },
   { id: 41,  n: 93,  text: "Stop letting yesterday get in the way of today.", name: "Rebecca Welton", role: "Club Owner" },
+  { id: 42,  n: 308,  text: "Find out before you flip out.", name: "Leslie Higgins", role: "Chief of Football Ops" },
   
 ];
